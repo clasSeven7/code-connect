7c741f8da7608e1ff53641c2c8f9ebdfa0ffe9b2
charles <ch4rle2.fh@gmail.com>
2024-06-03 15:39:06 -0300

    feat: Update database name and password in settings

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-
 from .models import Chat, Event, Post, Story, User
-
 
 def home(request):
     user = User.objects.all()
     story = Story.objects.all()
     post = Post.objects.all()
     event = Event.objects.all()
-    chat = Chat.objects.all()
+    chats = Chat.objects.all()
 
-    return render(request, 'home.html', {'user': user, 'story': story, 'post': post, 'event': event, 'chat': chat})
+    return render(request, 'home.html', {'user': user, 'story': story, 'post': post, 'event': event, 'chats': chats})
