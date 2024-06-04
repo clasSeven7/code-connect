afe33959c2554074e4ed1ae434f336390c8e845a
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-06-03 21:18:29 -0300

    feat: Update database name and password in settings
    This commit updates the database name and password in the settings.py file. The database name is changed from 'code_connect_db' to 'code-connect-db', and the password is changed from 'postgres-admin' to 'admin'. This change ensures that the correct database is used and the password is updated for security purposes.

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 
 from .models import Chat, Event, Post, Story
-=======
-from .models import Chat, Event, Post, Story, User
->>>>>>> refs/remotes/origin/main
 
 def home(request):
     story = Story.objects.all()
@@ -12,8 +8,4 @@ def home(request):
     event = Event.objects.all()
     chats = Chat.objects.all()
 
-<<<<<<< HEAD
-    return render(request, 'home.html', {'story': story, 'post': post, 'event': event, 'chat': chat})
-=======
-    return render(request, 'home.html', {'user': user, 'story': story, 'post': post, 'event': event, 'chats': chats})
->>>>>>> refs/remotes/origin/main
+    return render(request, 'home.html', {'story': story, 'post': post, 'event': event, 'chats': chats})
